d89fc36debf70a7f1dc5394676a77295145cc822
Chen Zhengrui <zhengrchan@gmail.com>
2020-05-24 16:11:49 +0800

    initialize and deploy webpack

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="test"> {{text}} </div>    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'abcdef'
+        }
+    }
+    
+}
+</script>
+
+<style>
+#test{
+    color:blue
+}
+</style>
